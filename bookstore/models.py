3f5db15789dc59012caa83274fc66c3ed0364749
saramoh20 <sara.mohammed009@outlook.com>
2022-08-14 09:36:33 +0300

    start blog project

---
+++ bookstore/models.py
@@ -0,0 +1,61 @@
+from cgi import print_exception
+import email
+from sre_constants import CATEGORY
+from statistics import mode
+from telnetlib import STATUS
+from unicodedata import category, name
+from django.db import models
+
+# Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=190, null=True)
+    email = models.CharField(max_length=190, null=True)
+    phone = models.CharField(max_length=190, null=True)
+    age = models.IntegerField(max_length=190, null=True)
+    deta_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name + self.email
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=190, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    CATEGORY = (
+        ('Classics', 'Classics'),
+        ('Comic Book', 'Comic Book'),
+        ('Fantasy', 'Fantasy'),
+        ('Horror', 'Horror')
+    )
+    name = models.CharField(max_length=190, null=True)
+    authoname = models.FloatField(max_length=190, null=True)
+    price = models.CharField(max_length=190, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    tag = models.ManyToManyField(Tag)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('pending', 'pending'),
+        ('Delivered', 'Deliverd'),
+        ('in progress', 'in progress'),
+        ('out of order', 'out of order')
+
+    )
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    book = models.ForeignKey(Book, null=True, on_delete=models.SET_NULL)
+    tag = models.ManyToManyField(Tag)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
---
+++ bookstore/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+from .models import *
+
+
+def dashboard(request):
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    t_orders = orders.count()
+    p_orders = orders.filter(status='pending').count()
+    d_orders = orders.filter(status='Delivered').count()
+    in_orders = orders.filter(status='in progress').count()
+    out_orders = orders.filter(status='').count()
+    context = {'customers': customers,
+               'orders': orders,
+               't_rders': t_orders,
+               'p_orders': p_orders,
+               'd_orders': d_orders,
+               'in_orders': in_orders,
+               'out_orders': out_orders}
+    return render(request, 'bookstore/dashboard.html', context)
+
+
+def books(request):
+    books = Book.objects.all()
+    return render(request, 'bookstore/books.html', {'book': books})
+
+
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    return (request, 'bookstore/customer.html')
